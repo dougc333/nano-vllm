02cb8282bfce2a7b2bc3614a27536bf8a877aa5b
dougc333 <dougchang25@gmail.com>
2026-08-12 04:17:09 -0700

    add moe verify

---
+++ test_progs/downloadqwen30b.py
@@ -0,0 +1,5 @@
+from huggingface_hub import snapshot_download
+
+#snapshot_download("Qwen/Qwen3-30B-A3B", local_dir="/Users/dc/nano-vllm/test_progs/Qwen3-30B-A3B")
+snapshot_download("Qwen/Qwen3-30B-A3B", local_dir="/Users/dc/nano-vllm/test_progs/Qwen3-0.6")
+
